c170244cff8aed90edefa1e18cfce63fed31ab4c
JiazeWang <jiazewang@csu.edu.cn>
2020-10-28 01:56:06 +0800

    Update pvnet.py

--- lib/visualizers/linemod/pvnet.py
+++ lib/visualizers/linemod/pvnet.py
@@ -24,7 +24,7 @@ class Visualizer:
         inp = img_utils.unnormalize_img(batch['inp'][0], mean, std).permute(1, 2, 0)
         inpnew = Image.open(high_resolution)
         kpt_2d = output['kpt_2d'][0].detach().cpu().numpy()
-        print(kpt_2d)
+        #print(kpt_2d)
         img_id = int(batch['img_id'][0])
         anno = self.coco.loadAnns(self.coco.getAnnIds(imgIds=img_id))[0]
         kpt_3d = np.concatenate([anno['fps_3d'], [anno['center_3d']]], axis=0)
@@ -42,6 +42,7 @@ class Visualizer:
         #corner_2d_gt = pvnet_pose_utils.project(corner_3d, K, pose_gt)
         corner_2d_pred = pvnet_pose_utils.project(corner_3d, K, pose_pred)
         #print(corner_2d_pred)
+        corner_2d_pred = corner_2d_pred 
         _, ax = plt.subplots(1)
         plt.axis('off')
         frame = plt.gca()
@@ -52,7 +53,7 @@ class Visualizer:
         #ax.add_patch(patches.Polygon(xy=corner_2d_gt[[5, 4, 6, 7, 5, 1, 3, 7]], fill=False, linewidth=1, edgecolor='g'))
         ax.add_patch(patches.Polygon(xy=corner_2d_pred[[0, 1, 3, 2, 0, 4, 6, 2]], fill=False, linewidth=2, edgecolor='r'))
         ax.add_patch(patches.Polygon(xy=corner_2d_pred[[5, 4, 6, 7, 5, 1, 3, 7]], fill=False, linewidth=2, edgecolor='r'))
-        ax.figure.savefig(name, bbox_inches='tight', dpi = 600)
+        ax.figure.savefig(name, bbox_inches='tight', dpi = 300)
         #plt.close('all')
         #plt.show()
 
