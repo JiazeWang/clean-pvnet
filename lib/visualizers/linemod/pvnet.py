dd763f67cebeba9479d8d7f916698217ff971f3d
JiazeWang <jiazewang@csu.edu.cn>
2020-10-28 01:50:41 +0800

    Update pvnet.py

--- lib/visualizers/linemod/pvnet.py
+++ lib/visualizers/linemod/pvnet.py
@@ -22,7 +22,7 @@ class Visualizer:
 
     def visualize(self, output, batch, name, high_resolution):
         inp = img_utils.unnormalize_img(batch['inp'][0], mean, std).permute(1, 2, 0)
-        #inpnew = Image.open(high_resolution)
+        inpnew = Image.open(high_resolution)
         kpt_2d = output['kpt_2d'][0].detach().cpu().numpy()
         print(kpt_2d)
         img_id = int(batch['img_id'][0])
@@ -47,7 +47,7 @@ class Visualizer:
         frame = plt.gca()
         frame.axes.get_yaxis().set_visible(False)
         frame.axes.get_xaxis().set_visible(False)
-        ax.imshow(inp)
+        ax.imshow(inpnew)
         #ax.add_patch(patches.Polygon(xy=corner_2d_gt[[0, 1, 3, 2, 0, 4, 6, 2]], fill=False, linewidth=1, edgecolor='g'))
         #ax.add_patch(patches.Polygon(xy=corner_2d_gt[[5, 4, 6, 7, 5, 1, 3, 7]], fill=False, linewidth=1, edgecolor='g'))
         ax.add_patch(patches.Polygon(xy=corner_2d_pred[[0, 1, 3, 2, 0, 4, 6, 2]], fill=False, linewidth=2, edgecolor='r'))
